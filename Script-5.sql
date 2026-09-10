d4c1d7ebb9abd5b2eb085caab24935284578f20b
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-10 18:02:20 -0300

    sql - dbeaver - init

--- Script-5.sql
+++ Script-5.sql
@@ -10,6 +10,8 @@ SELECT * FROM Artist a;
 SELECT a2.ArtistId, a2.Name, COUNT(*) AS Records FROM Album a INNER JOIN Artist a2 GROUP BY 1;
 SELECT a2.ArtistId, a2.Name, COUNT(*) AS Records FROM Album a INNER JOIN Artist a2 GROUP BY 1 ORDER BY Records LIMIT 3;
 
+SELECT * FROM Customer c LIMIT 10;
+SELECT COUNT(*) FROM Customer c2;
 
 
 
