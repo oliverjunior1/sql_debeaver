92567a3270ebee24acb13ce9f1fb29b951f8a8c2
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-10 18:22:33 -0300

    sql - dbeaver - init

--- Script-5.sql
+++ Script-5.sql
@@ -31,5 +31,7 @@ SELECT c.FirstName, c.LastName FROM Customer c
 	WHERE Company IS NULL AND c.FirstName IN 
 		(SELECT e.FirstName FROM Employee e);
 
+SELECT c.FirstName FROM Customer c INNER JOIN Employee e WHERE c.FirstName = c.FirstName;
+
 
 
