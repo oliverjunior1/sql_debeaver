08b7cf4d25ce43c121c6e526433a4fe7527e9751
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-10 18:00:28 -0300

    sql - dbeaver - init

--- Script-5.sql
+++ Script-5.sql
@@ -6,6 +6,10 @@ SELECT AlbumId, Title FROM Album a WHERE ArtistId IS NULL;
 
 SELECT * FROM Artist a;
 
+-- caracterizando os registros dos artistas
+SELECT a2.ArtistId, a2.Name, COUNT(*) AS Records FROM Album a INNER JOIN Artist a2 GROUP BY 1;
+SELECT a2.ArtistId, a2.Name, COUNT(*) AS Records FROM Album a INNER JOIN Artist a2 GROUP BY 1 ORDER BY Records LIMIT 3;
+
 
 
 
