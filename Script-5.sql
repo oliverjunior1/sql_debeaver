33e35a15339aae2db149f3b541706c79d470067e
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-10 17:19:54 -0300

    sql - dbeaver - init

--- Script-5.sql
+++ Script-5.sql
@@ -2,4 +2,5 @@ SELECT * FROM Album a;
 SELECT COUNT(*) FROM Album a;
 SELECT COUNT(*) AS Records FROM Album a;
 SELECT COUNT(*) FROM Album a WHERE Column1 is NULL;
+SELECT AlbumId, Title FROM Album a WHERE ArtistId IS NULL;
 
